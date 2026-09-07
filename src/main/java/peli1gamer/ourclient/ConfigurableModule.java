41716b51f8dde89852255bf9f842ef0de20b6f47
peli1gamer <leevilarkkonen@gmail.com>
2026-09-07 23:26:48 +0300

    Add extensible configurable module capability

---
+++ src/main/java/peli1gamer/ourclient/ConfigurableModule.java
@@ -0,0 +1,6 @@
+package peli1gamer.ourclient;
+
+/** Optional capability for modules that expose their settings to the client GUI. */
+public interface ConfigurableModule {
+    ModuleSettings settings();
+}
