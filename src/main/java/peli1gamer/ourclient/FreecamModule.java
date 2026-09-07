42226c183960b1f0d7ccc472acb71acda9498719
peli1gamer <leevilarkkonen@gmail.com>
2026-09-07 13:31:56 +0300

    feat: make freecam module toggleable

--- src/main/java/peli1gamer/ourclient/FreecamModule.java
+++ src/main/java/peli1gamer/ourclient/FreecamModule.java
@@ -1,8 +1,8 @@
 package peli1gamer.ourclient;
 
-public final class FreecamModule implements ClientModule {
-    @Override
-    public String id() {
-        return "freecam";
-    }
+public final class FreecamModule implements ToggleableModule {
+    private boolean enabled;
+    @Override public String id() { return "freecam"; }
+    @Override public boolean enabled() { return enabled; }
+    @Override public void setEnabled(boolean enabled) { this.enabled = enabled; }
 }
