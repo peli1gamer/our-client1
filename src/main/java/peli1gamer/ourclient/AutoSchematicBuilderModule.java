41bbfe5144396102a2a414a70da134a0c987383b
peli1gamer <leevilarkkonen@gmail.com>
2026-09-07 13:32:00 +0300

    feat: make schematic builder toggleable

--- src/main/java/peli1gamer/ourclient/AutoSchematicBuilderModule.java
+++ src/main/java/peli1gamer/ourclient/AutoSchematicBuilderModule.java
@@ -1,8 +1,8 @@
 package peli1gamer.ourclient;
 
-public final class AutoSchematicBuilderModule implements ClientModule {
-    @Override
-    public String id() {
-        return "auto-schematic-builder";
-    }
+public final class AutoSchematicBuilderModule implements ToggleableModule {
+    private boolean enabled;
+    @Override public String id() { return "auto-schematic-builder"; }
+    @Override public boolean enabled() { return enabled; }
+    @Override public void setEnabled(boolean enabled) { this.enabled = enabled; }
 }
