891c5bcea649a45e3967f753deaef3b2d2d21020
peli1gamer <leevilarkkonen@gmail.com>
2026-09-07 13:31:51 +0300

    feat: make tracers module toggleable

--- src/main/java/peli1gamer/ourclient/TracersModule.java
+++ src/main/java/peli1gamer/ourclient/TracersModule.java
@@ -1,8 +1,8 @@
 package peli1gamer.ourclient;
 
-public final class TracersModule implements ClientModule {
-    @Override
-    public String id() {
-        return "tracers";
-    }
+public final class TracersModule implements ToggleableModule {
+    private boolean enabled;
+    @Override public String id() { return "tracers"; }
+    @Override public boolean enabled() { return enabled; }
+    @Override public void setEnabled(boolean enabled) { this.enabled = enabled; }
 }
