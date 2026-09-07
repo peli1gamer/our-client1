b65c2d5ad55b90514b2aa4302b7d1f61217964e1
peli1gamer <leevilarkkonen@gmail.com>
2026-09-07 13:25:07 +0300

    feat: add auto schematic builder module scaffold

---
+++ src/main/java/peli1gamer/ourclient/AutoSchematicBuilderModule.java
@@ -0,0 +1,8 @@
+package peli1gamer.ourclient;
+
+public final class AutoSchematicBuilderModule implements ClientModule {
+    @Override
+    public String id() {
+        return "auto-schematic-builder";
+    }
+}
