aab9fc04cd79c9cde0c04ce7e4abac9a935604cc
peli1gamer <leevilarkkonen@gmail.com>
2026-09-08 16:53:15 +0300

    Add native module settings contract

--- src/main/java/peli1gamer/ourclient/ClientModule.java
+++ src/main/java/peli1gamer/ourclient/ClientModule.java
@@ -4,6 +4,12 @@ import net.minecraft.client.Minecraft;
 
 public interface ClientModule {
     String id();
+
     default void onClientTick(Minecraft client) {
+    }
+
+    /** Runtime settings exposed by the native Arson ClickGUI. */
+    default ModuleSettings settings() {
+        return new ModuleSettings();
     }
 }
