bf79c66353e32232a0e0f689d17af032b53f6593
peli1gamer <leevilarkkonen@gmail.com>
2026-09-09 01:04:56 +0300

    Harden Air Jump dead player handling

--- src/main/java/peli1gamer/ourclient/AirJumpModule.java
+++ src/main/java/peli1gamer/ourclient/AirJumpModule.java
@@ -26,7 +26,7 @@ public final class AirJumpModule implements ToggleableModule {
             previousJump = jump;
             return;
         }
-        if (client.player.isSpectator() || client.player.isPassenger()) {
+        if (!client.player.isAlive() || client.player.isSpectator() || client.player.isPassenger()) {
             previousJump = jump;
             return;
         }
